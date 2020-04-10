45b2699c2e9f4815c5c0879e302c8ec11e08c497
awmleer <awmleer@sparker.xyz>
2020-04-10 16:26:11 +0800

    basic feature

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {useEffect, useState, useRef, FC, createElement} from 'react'
+import * as ReactDOM from 'react-dom'
+
+interface Props {
+  tagName: string
+}
+
+export const Scoped: FC<Props> = (props) => {
+  const shadowRef = useRef<HTMLElement>()
+  const [shadow, setShadow] = useState<ShadowRoot>()
+  
+  useEffect(() => {
+    const shadow = shadowRef.current.attachShadow({mode: 'open'})
+    setShadow(shadow)
+  }, [])
+  
+  const host = createElement(props.tagName ?? 'div', {
+    ref: shadowRef
+  })
+  
+  return (
+    <>
+      {host}
+      <Inner shadow={shadow}>
+        {props.children}
+      </Inner>
+    </>
+  )
+}
+
+const Inner: FC<{
+  shadow?: ShadowRoot
+}> = (props) => {
+  console.log(props.shadow)
+  if (!props.shadow) return null
+  return ReactDOM.createPortal(props.children, props.shadow as any as Element)
+}
